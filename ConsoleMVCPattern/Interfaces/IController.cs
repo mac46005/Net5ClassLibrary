e5b8e9da1750a570b631583f61ec0412d0b8083a
Marco Preciado <mac46005@gmail.com>
2021-10-26 22:07:05 -0500

    added IView List to IController

--- ConsoleMVCPattern/Interfaces/IController.cs
+++ ConsoleMVCPattern/Interfaces/IController.cs
@@ -1,6 +1,9 @@
+using System.Collections.Generic;
+
 namespace ConsoleMVCPattern.Interfaces{
     public interface IController{
         void RequestView(IView view);
         IView ResponseView();
+        List<IView> ViewList {get;set;}
     }
 }
