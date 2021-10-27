82a2aa6783771eefbd7516d148d7353ecd391aec
Marco Preciado <mac46005@gmail.com>
2021-10-27 12:20:55 -0500

    Added Singleton Thread Safe example

---
+++ DesignPatterns.ClassLib/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DesignPatterns.ClassLib
+{
+    public class Class1
+    {
+    }
+}
---
+++ DesignPatterns.ClassLib/Classes/Singleton/SingletonThreadSafe.cs
@@ -0,0 +1,20 @@
+namespace DesignPatterns.ClassLib.Classes.Singleton{
+    public sealed class SingletonThreadSafe{
+        private static SingletonThreadSafe _instance = null;
+        private static readonly object _lock = new object();
+        SingletonThreadSafe(){}
+        public static SingletonThreadSafe SingleInstance{
+            get{
+                //The lock will allow only one thread at a time to access the block of code inside it.
+                // If there is alread a thread accessing the block of code inside the lock, the other threads
+                // will hold at this point until the first thread finishes her business inside that block
+                lock(_lock){
+                    if(_instance == null){
+                        _instance = new SingletonThreadSafe();
+                    }
+                    return _instance;
+                }
+            }
+        }
+    }
+}
