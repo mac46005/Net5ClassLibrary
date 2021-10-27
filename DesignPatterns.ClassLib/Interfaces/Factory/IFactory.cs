2211ef6f81071f0efe68e75764ddcc61069c5752
Marco Preciado <mac46005@gmail.com>
2021-10-27 18:26:54 -0500

    IFactory Interface

--- DesignPatterns.ClassLib/Interfaces/Factory/IFactory.cs
+++ DesignPatterns.ClassLib/Interfaces/Factory/IFactory.cs
@@ -1,5 +1,5 @@
 namespace DesignPatterns.ClassLib.Interfaces.Factory{
     public interface IFactory{
-        public T Manufacture<T,U>(U parameter);
+        public T CreateObject<T,U>(U parameter);
     }
 }
