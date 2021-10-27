65e9c9d981d0b4f016249589d0526f13f5533960
Marco Preciado <mac46005@gmail.com>
2021-10-27 12:25:44 -0500

    SingletonNotThreadSafe

---
+++ DesignPatterns.ClassLib/Classes/Singleton/SingletonNotThreadSafe.cs
@@ -0,0 +1,17 @@
+namespace DesignPatterns.ClassLib.Classes.Singleton{
+    public sealed class SingletonNotThreadSafe{
+        private static SingletonNotThreadSafe _instance = null;
+        private SingletonNotThreadSafe(){}
+        public static SingletonNotThreadSafe SingleInstance{
+            get{
+                if(_instance == null){
+                    // It is possible that two different threads reach this point at the same time,
+                    // so more than one instance can be created in a situation like this, because both will
+                    // pass the condition about _instance being null.
+                    _instance = new SingletonNotThreadSafe();
+                }
+                return _instance;
+            }
+        }
+    }
+}
