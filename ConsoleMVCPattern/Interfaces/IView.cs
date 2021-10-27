c04992c8832e74612b89384fe1a26a3ea8547ca6
Marco Preciado <mac46005@gmail.com>
2021-10-27 14:09:43 -0500

    Added People.ClassLib

--- ConsoleMVCPattern/Interfaces/IView.cs
+++ ConsoleMVCPattern/Interfaces/IView.cs
@@ -1,7 +1,6 @@
 namespace ConsoleMVCPattern.Interfaces{
     public interface IView
     {
-        int GetID {get;set;}
-        void DisplayId();
+        void Display();
     }
 }
---
+++ People.ClassLib/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace People.ClassLib
+{
+    public class Class1
+    {
+    }
+}
