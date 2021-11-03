ff898db719b3d456eb5fc2fe94e214ede38003c4
Marco Preciado <mac46005@gmail.com>
2021-11-03 04:29:09 -0500

    Find some way to subscribe to the event change when controller is being changed

--- ConsoleMVCPattern/Classes/ConsoleMVC_Worker.cs
+++ ConsoleMVCPattern/Classes/ConsoleMVC_Worker.cs
@@ -1,9 +1,17 @@
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
 namespace ConsoleMVCPattern.Classes{
     public class ConsoleMVC_Worker : BackgroundService
     {
+
+
+        private void SubscribeToEvents(){
+           GetType().Assembly.GetTypes()
+           .Where(t => t.IsClass); 
+        }
+
         protected override Task ExecuteAsync(CancellationToken stoppingToken)
         {
             throw new System.NotImplementedException();
