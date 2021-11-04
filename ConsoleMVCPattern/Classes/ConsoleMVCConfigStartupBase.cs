03ea0b5023c8da25302c810ef3fd6293b7ea0876
Marco Preciado <mac46005@gmail.com>
2021-11-03 22:17:11 -0500

    Context,Services

--- ConsoleMVCPattern/Classes/ConsoleMVCConfigStartupBase.cs
+++ ConsoleMVCPattern/Classes/ConsoleMVCConfigStartupBase.cs
@@ -7,40 +7,11 @@ namespace ConsoleMVCPattern.Classes
 {
     public class ConsoleMVCStartupBase
     {
-        
-        private string _consoleControllerStartUp;
-        public void SetConsoleControllerStartUp(string consoleControllerStartUp){
-            if(!consoleControllerStartUp.EndsWith("ConsoleController")){
 
-            }
-            else{
-                _consoleControllerStartUp = consoleControllerStartUp;
-            }
-        }
-        public virtual IServiceCollection ConfigureServices(IServiceCollection services)
-        {
-
-            if (services == null)
-            {
-                services = new ServiceCollection();
-                GetType().Assembly.GetTypes()
-                .Where(t => t.IsClass)
-                .Where(t => t.Name.EndsWith("ConsoleController") | t.Name.EndsWith("ConsoleView") | t.Name.EndsWith("Model"))
-                .ToList()
-                .ForEach(t => services.AddTransient(t.GetInterface(t.Name), t));
-                return services;
-            }
-            else
-            {
-                return services;
-            }
-
-        }
-        
-        public virtual IHostBuilder CreateBuilder(string[] args)
-        {
-            return Host
-            .CreateDefaultBuilder(args);
+        private IHostBuilder ThisHostBuilder {get;}
+        static void AddHostedSevice<T>(T host,string[] args){
+            Host.CreateDefaultBuilder(args)
+            .ConfigureServices((context,services)=> {});
         }
     }
 }
